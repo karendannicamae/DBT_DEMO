161fddb4535013a3df3ed5f5a2a9150fa2f0df95
karendmacuna@gmail.com <karendmacuna@gmail.com>
2026-05-28 10:01:26 +0000

    date_utils

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -7,12 +7,7 @@ WITH CTE AS (
     WHEN DAYNAME(TO_TIMESTAMP(started_at)) IN ('Sat','Sun') THEN 'WEEKEND'
     ELSE 'BUSINESSDAY' 
     END AS DAYTYPE,
-    CASE
-    WHEN MONTH(TO_TIMESTAMP(started_at)) IN (12,1,2) THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP(started_at)) IN (3,4,5) THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP(started_at)) IN (6,7,8) THEN 'SUMMER'
-    ELSE 'AUTUMN'
-    END AS SEASON_OF_YEAR
+    {{get_season('STARTED_AT')}} AS SEASON_0F_YEAR 
 
     FROM {{ source('demo', 'bike') }}
 )
